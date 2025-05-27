5351ea0d9c269bcd0991b74b294ef0dba3b96c4e
Ramesh <116292847+RameshDumala1@users.noreply.github.com>
2025-05-27 12:54:03 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,12 +1,44 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
-  default = "devops-eks-cluster"
+  type        = string
+  description = "EKS cluster name"
 }
 
-variable "vpc_id" {}
-variable "subnets" {
-  type = list(string)
+variable "vpc_id" {
+  description = "VPC ID where EKS cluster will be created"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subnets for EKS cluster"
+}
+
+variable "node_instance_types" {
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "node_min_size" {
+  default = 1
+}
+
+variable "node_max_size" {
+  default = 2
+}
+
+variable "node_desired_size" {
+  default = 1
+}
+
+variable "node_disk_size" {
+  default = 20
+}
+
+variable "iam_user_name" {
+  default = "eks-admin-user"
 }
