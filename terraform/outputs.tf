a05c10fe1e4428b93b8e2c70611b89286a27ad93
Ramesh <116292847+RameshDumala1@users.noreply.github.com>
2025-05-27 12:56:43 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,18 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
 output "kubeconfig" {
-  value = module.eks.kubeconfig
+  value = <<EOT
+Run the following command to configure your kubeconfig:
+
+aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}
+EOT
 }
